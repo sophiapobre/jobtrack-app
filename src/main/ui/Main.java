fa3e03b9c68bd290b2a136415037479555954988
Sophia Pobre <sophiapobre@gmail.com>
2023-02-25 14:42:14 -0800

    Removed unnecessary import statement

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,18 +1,12 @@
 package ui;
 
-import java.io.FileNotFoundException;
-
 // Represents the class containing the main method
 public class Main {
 
     /*
-     * EFFECTS: creates a new JobTrackApp, prints error message if file requested by user cannot be found
+     * EFFECTS: creates a new JobTrackApp
      */
     public static void main(String[] args) {
-        try {
-            new JobTrackApp();
-        } catch (FileNotFoundException fnfe) {
-            System.out.println("ERROR: Cannot load application because the file was not found.");
-        }
+        new JobTrackApp();
     }
 }
